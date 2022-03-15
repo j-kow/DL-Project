9d73b468a528277c2af651b63f952efcb5156f13
j-kow <92652871+j-kow@users.noreply.github.com>
2022-03-15 08:46:26 +0100

    Added documentation

--- src/places365_project/pipelines/dataset_creation/nodes.py
+++ src/places365_project/pipelines/dataset_creation/nodes.py
@@ -10,6 +10,17 @@ except ImportError:
 
 
 def create_data_loader(data_dir: str, batch_size: int, num_workers: int) -> torch.utils.data.DataLoader:
+    """Creates pytorch data loader from places365 dataset
+
+        :param data_dir: Path to the root of processed dataset
+        :type data_dir: str
+        :param batch_size: Number of images in a batch
+        :type batch_size: int
+        :param num_workers: How many subprocesses are used for data loading
+        :type num_workers: int
+        :return: Data loader for places365 dataset
+        :rtype: torch.utils.data.DataLoader
+        """
     dataset = tv.datasets.ImageFolder(
         data_dir,
         transform=tv.transforms.Compose([
@@ -22,6 +33,13 @@ def create_data_loader(data_dir: str, batch_size: int, num_workers: int) -> torc
 
 
 def prune(data_dir: str, prune_frac: float):
+    """Deletes set percentage of samples from each class
+
+    :param data_dir: Path to the root of processed dataset
+    :type data_dir: str
+    :param prune_frac: Percentage of samples to stay in dataset
+    :type data_dir: str
+    """
     classes = os.listdir(data_dir)
     for class_name in classes:
         class_path = os.path.join(data_dir, class_name)
@@ -33,6 +51,24 @@ def prune(data_dir: str, prune_frac: float):
 
 
 def refactor_data_structure(raw_dir: str, new_dir: str, delete_old: bool):
+    """Refactors directory structure to match standard structure for image dataset.
+    End result will look like this:
+
+    root
+    |- class1
+       |- image1
+       |- image2
+       ...
+    |- class2
+    ...
+
+    :param raw_dir: Path to the root of original dataset
+    :type raw_dir: str
+    :param new_dir: Path to the root of processed dataset. Entire dataset will be refactored to this path.
+    :type new_dir: str
+    :param delete_old: If true old dataset will be deleted from memory. Use true if memory is limited
+    :type delete_old: bool
+    """
     dirs = os.listdir(raw_dir)
 
     if delete_old:
